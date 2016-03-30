6df388e416d7bdf3207e2855cd9eae7bb98cf2a9
Steve Harvey <sgh@vex.net>
2016-03-30 00:14:03 -0400

    Kernels generated by aniso_kern.py are centred. (Bug fix).

--- aniso_kern.py
+++ aniso_kern.py
@@ -18,15 +18,28 @@ def generate_aniso_gauss_kernel(w, sigmas, angle):
     kernel
 
     """
-    m = np.zeros((w, w), dtype=np.float)
+    b = w+w+1
+    m = np.zeros((b, b), dtype=np.float)
     d = (w-1) / 2
-    m[d, d] = 1.0
-    return scipy.ndimage.rotate(scipy.ndimage.gaussian_filter(m, sigmas), angle)
+    m[w, w] = 1.0
+    #FIXME: document why this is correct
+    rotated = scipy.ndimage.rotate(scipy.ndimage.gaussian_filter(m, sigmas), angle)
+    #print "rotated size is", rotated.shape
+    #print rotated
+    row, col = divmod(rotated.argmax(), rotated.shape[0])
+    #print row, col
+    return rotated[row-d:row+d+1,col-d:col+d+1]
 
 if __name__ == '__main__':
     import sys
 
-    w, sigma1, sigma2, angle = [float(s) for s in sys.argv[1:]]
+    np.set_printoptions(linewidth=1000, precision=3)
+
+    try:
+        w, sigma1, sigma2, angle = [float(s) for s in sys.argv[1:]]
+    except ValueError:
+        print "Use: %s width sigma1 sigma2 angle" % sys.argv[0]
+        sys.exit(1)
 
     w = int(w)
 
